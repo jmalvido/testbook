845a4726435fe0fa143b2fef188c2af47d13693a
Jaime Malvido <32091018+jmalvido@users.noreply.github.com>
2021-01-28 09:43:24 +0100

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -7,8 +7,7 @@ class Database {
 	}
 
 	function connect(){
-		Database();
-		//$this->user="ba7ef97bc41990";$this->pass="f1ed729f";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_02b2719c8ff0657";
+		$this->user="ba7ef97bc41990";$this->pass="f1ed729f";$this->host="eu-cdbr-west-03.cleardb.net";$this->ddbb="heroku_02b2719c8ff0657";
 		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
 		$con->query("set sql_mode=''");
 		return $con;
